c503bd5bfd8f19df8fad2b2906dfe55c9e7b47ba
Taiane Rodrigues <taianeapdarodrigues@gmail.com>
2022-03-30 08:49:35 -0300

    Add files via upload

---
+++ Pilha.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorreDeHanoi_Trabalho
+{
+    class Pilha
+    {
+        /// <summary>
+        /// Aponta para o topo da pilha
+        /// </summary>
+        public NoPilha Topo { get; set; }
+
+        /// <summary>
+        /// Construtora padrão
+        /// </summary>
+        public Pilha()
+        {
+            Topo = null;
+        }
+
+        /// <summary>
+        /// Empilha o valor informado
+        /// </summary>
+        /// <param name="valor">Valor a ser empilhado</param>
+        public void Push(int valor)
+        {
+            NoPilha novo = new NoPilha(valor);
+            novo.Proximo = Topo;
+            Topo = novo;
+        }
+
+        public int Pop()
+        {
+            int valorRetorno = -1;
+
+            if (Topo != null)
+            {
+                valorRetorno = Topo.Info;
+                Topo = Topo.Proximo;
+            }
+
+            return valorRetorno;
+        }
+
+        public void Imprimir()
+        {
+            if (Topo != null)
+            {
+                // Garantir que o Primeiro não perca a referência
+                NoPilha navegador = Topo;
+
+                while (navegador != null)
+                {
+                    Console.Write(navegador.Info + ", ");
+                    navegador = navegador.Proximo;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorreDeHanoi_Trabalho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilha T01 = new Pilha();
+            //Pilha T02 = new Pilha();
+            //Pilha T03 = new Pilha();
+
+            T01.Push(1);
+            T01.Push(2);
+            T01.Push(3);
+            T01.Push(4);
+            T01.Push(5);
+            T01.Push(6);
+            T01.Pop();
+            T01.Pop();
+            T01.Pop();
+            T01.Pop();
+            T01.Pop();
+
+            T01.Imprimir();
+
+            /* //T01.Imprimir();
+             Console.WriteLine("---------------------");
+             Console.WriteLine("Inicio");
+             Console.WriteLine();
+             Console.WriteLine("1º Torre: ");
+             T01.Imprimir();
+             Console.WriteLine();
+             Console.WriteLine("2º Torre: ");
+             T02.Imprimir();
+             Console.WriteLine();
+             Console.WriteLine("3º Torre: ");
+             T03.Imprimir();
+             Console.WriteLine();
+             Console.WriteLine("---------------------");
+             int num = 3;
+
+             TorreDeHanoi(num, T01, T02, T03);
+             Console.WriteLine();
+             Console.WriteLine("Numero de movimentos: " + ((Math.Pow(2,num))-1));*/
+
+        }
+
+        /*public static void TorreDeHanoi(int num, Pilha t1, Pilha t2, Pilha t3)
+        {
+            if (num > 0)
+            {
+                TorreDeHanoi(num - 1, t1, t3, t2);
+                t3.Push(t1.Pop());
+
+                Console.WriteLine();
+                Console.WriteLine("1º Torre: ");
+                t1.Imprimir();
+                Console.WriteLine();
+                Console.WriteLine("2º Torre: ");
+                t2.Imprimir();
+                Console.WriteLine();
+                Console.WriteLine("3º Torre: ");
+                t3.Imprimir();
+                Console.WriteLine();
+                Console.WriteLine("---------------------");
+                TorreDeHanoi(num-1, t2, t1, t3);
+
+            }
+        }*/
+        
+    }
+}
